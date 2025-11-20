f683293e7b9601fc7eb538eab6afd9a16e14d1a9
BILAL SAHLI <bilal.sahli@statnett.no>
2025-11-20 10:38:49 +0100

    update main

--- courrier_optimizer/__main__.py
+++ courrier_optimizer/__main__.py
@@ -1,8 +1,15 @@
 from Services.ReadCsvFile import ReadCsvFile
-
+from Utils.Validate import Validate
 def main():
     csv_file = ReadCsvFile("input1.csv")
-    csv_file.readCsv()
+    inputs_from_input_csv = csv_file.readCsv()
+    validate = Validate
+    for input in inputs_from_input_csv:
+        if validate.validate_name(input.customer):
+            print("OK")
+        else:
+            print("Not Ok")
+        print(input)
 
 if __name__ == '__main__':
     main()
